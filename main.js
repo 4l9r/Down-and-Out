59792657ed01d49421136a2ec48eccf199fb95ec
4of92000 <4of92000@gmail.com>
2015-02-11 13:30:24 -0600

    Add some JS
    NOT DONE.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+/*
+This is the main JS code for the game, using features of <canvas>.
 
+It is imported into main.html.
+*/
+
+function main() {
+  // main game function
+  // this should be at the end of the file
+  var canvas = document.getElementById("game");
+}
